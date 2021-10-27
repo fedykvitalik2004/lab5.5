b2520fea79b69b3d588cedba50583bf3a4a2eec7
Vitalii Fedyk <vitalii.fedyk.it.2021@lpnu.ua>
2021-10-27 20:52:06 +0300

    First

---
+++ lab5.5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+double tP(int r, int n);
+
+int main()                                                                                                                                    
+{
+	int n, S = 0;
+	cout << "n="; cin >> n;
+	for (int rr = 0; rr <= n; rr++)
+	{
+		for (int nn = 0; nn <= rr; nn++)
+			cout << tP(nn, rr )<<" ";
+		cout << endl;
+	}
+}
+
+double tP(int n, int r)
+{
+	if (n == 0 || n == r)
+		return 1;
+	else
+		return tP(n-1, r-1) + tP(n, r-1 );
+}
